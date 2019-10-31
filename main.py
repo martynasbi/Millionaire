a7eba6681bdb5df0c783b71b070af361180c53d9
martynasbi <mabielskis@gmail.com>
2019-10-31 11:47:54 +0200

    Transfer questions to separate .txt files

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import random
+import json
+
+def read_questions(klausimynas):
+    with open(klausimynas, encoding='utf-8') as f:
+        klausimu_eilutes = f.read().splitlines()
+    klausimai = [json.loads(klausimas) for klausimas in klausimu_eilutes ]
+    return klausimai
 
 def ar_zaisti(start):
     beginning = 0
     while True:
-        if start == str(beginning):
+        if int(start) == beginning:
             atsakymas = input("\nAr norite pradėti žaidimą: TAIP / NE\n").upper()
         else:
             atsakymas = input("\nAr norite kartoti žaidimą: TAIP / NE\n").upper()
         if "T" in atsakymas:
             pradeti_zaidima()
         elif "N" in atsakymas:
-            print ("\nIki. Lauksime Jūsų sugrįžtant!")
+            print ("\nIki. Lauksime Jūsų sugrįžtant!\n")
             exit()
         else:
             continue
@@ -30,75 +37,9 @@ def pradeti_zaidima():
     laimeti_kartai = 0
     klausimo_numeris = 0
      
-    klausimynas_easy = [ { "klausimas": "Kas atstovauja indų kino meną?",
-                           "atsakymas": "Bolivudas",
-                           "atsakymai": ["Bolivudas",
-                                         "Holivudas",
-                                         "Golivudas",
-                                         "Indivudas" ] },
-                         
-                         { "klausimas": "Kokios šalies veikėjas buvo Leninas?",
-                           "atsakymas": "Rusijos",
-                           "atsakymai": ["Rusijos",
-                                         "Ukrainos",
-                                         "Čekijos",
-                                         "Gruzijos" ]},
-                         
-                         { "klausimas": "Kaip vadinama istorijos disciplina padedanti nustatyti įvykių eiliškumą ir datas?",
-                           "atsakymas": "Chronologija",
-                           "atsakymai": ["Chronologija",
-                                         "Enkaustika",
-                                         "Bonistika",
-                                         "Paleografija" ]},
-                         
-                         { "klausimas": "Kaip vadinamas didžiausio ir mažiausio dydžio skirtumas?",
-                           "atsakymas": "Amplitudė",
-                           "atsakymai": ["Amplitudė",
-                                         "Atogrąža",
-                                         "Speigratis",
-                                         "Brizas" ]}]
-
-    klausimynas_medium = [ { "klausimas": "Kuris iš šių rašytojų ekspresionistas?",
-                             "atsakymas": "Boruta",
-                             "atsakymai": ["Boruta",
-                                           "Simonaitytė",
-                                           "Cvirka",
-                                           "Mieželaitis" ] },
-                           
-                           { "klausimas": "Kurio romano autorius yra Viktoras Hugo?",
-                             "atsakymas": "Vargdieniai",
-                             "atsakymai": ["Vargdieniai",
-                                           "Kaimiečiai",
-                                           "Kalniečiai",
-                                           "Turtuoliai" ]},
-                           
-                           { "klausimas": "Kaip paleontologijoje vadinami \"velnio nagai\"?",
-                             "atsakymas": "Belemnitai",
-                             "atsakymai": ["Belemnitai",
-                                           "Amonitai",
-                                           "Trilobitai",
-                                           "Stalagmitai" ]}]
-
-    klausimynas_hard = [ { "klausimas": "\"Šimtas zuikių susirinko, Net žalia girelė linko\". Kas poemėlės autorius?",
-                           "atsakymas": "Eduardas Mieželaitis",
-                           "atsakymai": ["Eduardas Mieželaitis",
-                                         "Martynas Vainilaitis",
-                                         "Kostas Kubilinskas",
-                                         "Vytė Nemunėlis" ] },
-                         
-                         { "klausimas": "Kurioje valstybėje sesoto kalba yra valstybinė?",
-                           "atsakymas": "Lesote",
-                           "atsakymai": ["Lesote",
-                                         "Somalyje",
-                                         "Sesote",
-                                         "Libijoje" ]},
-                         
-                         { "klausimas": "Kuris iš šių metalų netirpsta net \"karališkajame vandenyje\"?",
-                           "atsakymas": "Iridis",
-                           "atsakymai": ["Iridis",
-                                         "Sidabras",
-                                         "Auksas",
-                                         "Platina" ]}]
+    klausimynas_easy = read_questions("easy_klausimai.txt")
+    klausimynas_medium = read_questions("medium_klausimai.txt")
+    klausimynas_hard = read_questions("hard_klausimai.txt")
     
     for klausimas in klausimynas_easy.copy():
         klausimynas_easy, laimeta_suma, bandymu_skaicius, klausimo_numeris, laimeti_kartai = uzduoti_klausima(
@@ -115,12 +56,10 @@ def uzduoti_klausima(klausimynas, laimeta_suma, bandymu_skaicius, klausimo_numer
     laimimos_sumos = [1000, 3000, 6000, 15000, 30000, 50000, 75000, 130000, 250000, 440000]
     klausimo_numeriai = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     atsakymu_raides = ["A", "B", "C", "D"]
-    
     klausimas = (random.choice(klausimynas))
     klausimynas.remove(klausimas)
     pirmas_klausimas = klausimo_numeriai[klausimo_numeris]
     print(str(pirmas_klausimas) + ". " + klausimas["klausimas"])
-    
     random_atsakymai = random.sample(klausimas["atsakymai"], len(klausimas["atsakymai"]))
     print("\nAtsakymų variantai:")
 
@@ -129,9 +68,7 @@ def uzduoti_klausima(klausimynas, laimeta_suma, bandymu_skaicius, klausimo_numer
     print()
 
     while True:
-        
         while True:
-            
             if bandymu_skaicius == 0:
                 print("Jūs išnaudojote visus bandymus.\n")
                 if laimeta_suma >= 180000:
@@ -151,14 +88,11 @@ def uzduoti_klausima(klausimynas, laimeta_suma, bandymu_skaicius, klausimo_numer
                     print("Deja šį kartą Jūs nieko nelaimėjote.\n\nAčiū už dalyvavimą, bandykite kitą kartą!")
                     print("*****************************************")
                     ar_zaisti(1)
-                    
             atsakymo_raide = input("Kai norėsite stabdyti žaidimą, pasirinkite: S.\nPasirinkite atsakymo variantą: A, B, C ar D.\n").upper()
             if atsakymo_raide in atsakymu_raides:
-                
                 atsakymas = random_atsakymai[ord(atsakymo_raide)-65]
                 print("Jūs pasirinkote atsakymą:",atsakymas)
                 break
-                
             elif atsakymo_raide == "S":
                 if laimeta_suma == 0:
                     print("*************************************************************************************************")
@@ -172,7 +106,6 @@ def uzduoti_klausima(klausimynas, laimeta_suma, bandymu_skaicius, klausimo_numer
                     ar_zaisti(1)
             else:
                 print("Nėra tokio pasirinkimo.\n")
-                
         if klausimas["atsakymas"] == atsakymas:
             print("\nJūsų atsakymas yra teisingas!")
             laimeta_suma += laimimos_sumos[laimeti_kartai]
@@ -186,7 +119,6 @@ def uzduoti_klausima(klausimynas, laimeta_suma, bandymu_skaicius, klausimo_numer
                 print("$Sveikiname, Jūs laimėjote milijoną!$")
                 print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
                 ar_zaisti(1)
-                 
         else:
             bandymu_skaicius -= 1
             if bandymu_skaicius > 1:
